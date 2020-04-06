5080b7936e1d613a703f619d3c78d7f6b091de1d
Stuart <contactstuartonline@gmail.com>
2020-04-06 22:06:05 +0100

    Particles
    Got Particles.js working!

--- src/App.js
+++ src/App.js
@@ -10,7 +10,17 @@ import Particles from 'react-particles-js';
 function App() {
   return (
     <div className="App">
-        {/* <Particles className='particles' /> */}
+        <Particles className='particles' 
+            params={{
+              particles: {
+                    color: {value: "#000000",},
+                    number: {value: "100",},
+                    size: {value: "10"},
+                    opacity: {value: "0.6"},
+                    line_linked: {color:{value: "#000000",}}
+                  }
+                }}
+          />
         <Navigation />
         <Logo />
         <ImageLinkForm />
