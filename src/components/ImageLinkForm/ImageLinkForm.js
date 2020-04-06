b288456ba2f9c54e1cd8a8854a10c0e43d3d1cee
Stuart <contactstuartonline@gmail.com>
2020-04-06 21:43:18 +0100

    Styling/Particles
    Added some styling, tried and failed to get the particles.js to work as a background

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -3,10 +3,14 @@ import React from 'react';
 const ImageLinkForm = () => {
     return(
         <div>
-            <form>
-                <label for="fname">Image Link:</label>
-                <input type="text" id="URL" name="URL" placeholder="website.com/image.jpg"/>
-            </form>
+            <p>
+                {'Enter the link of a picture and detect all the faces!'}
+            </p>
+            <div className='form center'>
+                <input className='f4 pa2 w-30 center' type="text" id="URL" name="URL" placeholder="www.website.com/your-image.jpg"/>
+                <br/>
+                <button className='f4 pa2 w-30 center link ph3 pv2 dib white bg-light-purple'>Detect Faces</button>
+            </div>
         </div>
     );
 }
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,7 +6,7 @@ const Logo = () => {
     return(
         <div className='ma4 mt0'>
             <Tilt className="Tilt" options={{ max : 25 }} style={{ height: 250, width: 250 }} >
-                <div className="Tilt-inner"><img src={brain} alt='brain logo'/><strong>Facial Recognition</strong></div>
+                <div className="Tilt-inner"><img src={brain} alt='brain logo'/><strong className="f10">Face Recognition</strong></div>
             </Tilt>
         </div>
     );
