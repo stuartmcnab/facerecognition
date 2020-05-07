d19cdeef4ddc4dc916a843f269bed2ce3a8856d8
Stuart <contactstuartonline@gmail.com>
2020-05-07 15:07:23 +0100

    update

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
-import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+// import Clarifai from 'clarifai';
+// import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 
 
-const app = new Clarifai.App({
-  apiKey: '37e7c3af61094d51b672b09e93d9b2e1'
- });
+// const app = new Clarifai.App({
+//   apiKey: '37e7c3af61094d51b672b09e93d9b2e1'
+//  });
 
 function App() {
   return (
@@ -26,55 +26,10 @@ function App() {
                   }
                 }}
           />
-        calculateFaceLocation = (data) => {
-            const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-            const image = document.getElementById('inputimage');
-            const width = Number(image.width);
-            const height = Number(image.height);
-            return {
-              leftCol: clarifaiFace.left_col * width,
-              topRow: clarifaiFace.top_row * height,
-              rightCol: width - (clarifaiFace.right_col * width),
-              bottomRow: height - (clarifaiFace.bottom_row * height)
-            }
-          }
-
-  displayFaceBox = (box) => {
-    this.setState({box: box});
-  }
-
-  onInputChange = (event) => {
-    this.setState({input: event.target.value});
-  }
-
-  onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input});
-    app.models
-      .predict(
-        Clarifai.FACE_DETECT_MODEL,
-        this.state.input)
-      .then(response => {
-        if (response) {
-          fetch('http://localhost:3000/image', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-              id: this.state.user.id
-            })
-          })
-            .then(response => response.json())
-            .then(count => {
-              this.setState(Object.assign(this.state.user, { entries: count}))
-            })
-
-        }
-        this.displayFaceBox(this.calculateFaceLocation(response))
-      })
-      .catch(err => console.log(err));
-  }
+       
         <Navigation />
         <Logo />
-        <FaceRecognition />
+        {/* <FaceRecognition /> */}
         <ImageLinkForm />
     </div>
   );
