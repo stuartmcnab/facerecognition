73bb377af681726a63836dbb7807b7e38f25d585
Stuart <contactstuartonline@gmail.com>
2020-05-07 11:56:47 +0100

    Clarifai integration - baby steps

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return(
         <div>
             <p className="f4">
                 Enter the link of a picture and detect all the faces!
             </p>
             <div className='form center'>
-                <input className='f4 pa2 w-30 center' type="text" id="URL" name="URL" placeholder="www.website.com/your-image.jpg"/>
+                <input className='f4 pa2 w-30 center' type="text" id="URL" name="URL" placeholder="www.website.com/your-image.jpg" onChange={onInputChange}/>
                 <br/>
-                <button className='f4 pa2 w-30 center link ph3 pv2 dib white bg-light-purple'>Detect Faces</button>
+                <button className='f4 pa2 w-30 center link ph3 pv2 dib white bg-light-purple' onClick={onButtonSubmit}>Detect Faces</button>
             </div>
         </div>
     );
